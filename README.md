951bc3ac2ef594d34fef3c9fb00c84a6049fc9fe
map220v <map220v300@gmail.com>
2021-10-15 21:24:08 +0300

    libgdx 1.10.0

---
+++ README.md
@@ -0,0 +1,7 @@
+Libs Source: https://repo1.maven.org/maven2/com/badlogicgames/gdx/
+
+Libs version: 1.10.0
+
+Libs arch: armeabi-v7a, arm64-v8a
+
+Libs: gdx, gdx-box2d, gdx-bullet, gdx-freetype
---
+++ app/src/main/java/com/mygdx/game/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.mygdx.game;
+
+import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useImmersiveMode = true;
+		initialize(new MyGdxGame(), config);
+    }
+}
